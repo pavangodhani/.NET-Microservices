0e0014012cd71ce3114891fdf742be07141c7bb1
Pavan Godhani <pavangodhani@Pavans-MacBook-Pro.local>
2022-12-04 12:59:28 +0530

    Add db context

---
+++ PlatformService/PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PlatformService.DomainModels;
+
+namespace PlatformService.Data
+{
+	public interface IPlatformRepo
+	{
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatform(Platform plat);
+    }
+}
+
---
+++ PlatformService/PlatformService/DomainModels/Platform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.DomainModels
+{
+	public class Platform
+	{
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Cost { get; set; }
+
+        public Platform(string id, string name, string publisher, string cost)
+        {
+            Id = id;
+            Name = name;
+            Publisher = publisher;
+            Cost = cost;
+        }
+    }
+}
+
